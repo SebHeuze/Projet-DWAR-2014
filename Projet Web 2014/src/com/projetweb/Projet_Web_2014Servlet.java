ef673d0fe24c7429373f0044c7e7d50d35929c51
sebastien <sebastien>
2014-03-10 15:54:11 +0100

    Commit projet GoogleApp Engine

---
+++ Projet Web 2014/src/com/projetweb/Projet_Web_2014Servlet.java
@@ -0,0 +1,11 @@
+package com.projetweb;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Projet_Web_2014Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
