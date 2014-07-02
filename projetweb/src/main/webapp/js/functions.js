1edf356cde9de5ab58ca359eb0b2c8e0e33cc98b
sebastien <sebastien>
2014-07-02 14:40:54 +0200

    Images en local

--- projetweb/src/main/webapp/js/functions.js
+++ projetweb/src/main/webapp/js/functions.js
@@ -1,1 +1,330 @@
-function createMarker(e,t,n,r,i){var s="<b>"+n+"</b><br>"+r;var o=new google.maps.Marker({position:t,map:e,shadow:iconShadow,icon:getMarkerImage(i),shape:iconShape,title:n,zIndex:Math.round(t.lat()*-1e5)<<5});o.myname=n;google.maps.event.addListener(o,"click",function(){infowindow.setContent(s);infowindow.open(e,o)});return o}function createTanMarker(e,t,n,r){latlng=new google.maps.LatLng((n.k-t.k)/2+t.k,(n.A-t.A)/2+t.A);var i=new google.maps.Marker({position:latlng,map:e,icon:new google.maps.MarkerImage("https://www.tan.fr/ewp/images/pictos_lignes/"+r+".gif",null,null,null,new google.maps.Size(14,14)),title:"Ligne"});return i}function setDirectionsBus(e,t,n){var r=[];var i=[];var s=[];var o=10;var u=0;var a=e.length>0;var f;while(a){var l=[];var c=0;var f=e[u+1].travelMode;var h=e[u+1].ligne;for(var p=u;p<e.length;p++){if(p!=u&&e[p].travelMode!=f){s.pop();break}c++;l.push({location:new google.maps.LatLng(e[p].latitude,e[p].longitude),stopover:true});s.push(e[p].ligne);if(c==o){s.pop();break}}u+=c;r.push(l);i.push(f);a=u<e.length;u--}var d;var v=[{}];var m=0;for(var g in r){if(i[g]=="BUS"||i[g]=="TRAM"){var y=[];for(var b in r[g]){y.push(r[g][b].location)}var w=new google.maps.Polyline({strokeColor:transitColor,strokeOpacity:.8,path:y,strokeWeight:5,map:map});busPaths.push(w)}}for(var E=0;E<r.length;E++){var S=r[E].length-1;var x=r[E][0].location;var T=r[E][S].location;var N=[];N=r[E];N.splice(0,1);N.splice(N.length-1,1);var C={origin:x,destination:T,waypoints:N,travelMode:google.maps.TravelMode.WALKING};(function(e){directionsService.route(C,function(t,n){if(n==window.google.maps.DirectionsStatus.OK){var o={order:e,result:t};v.push(o);m++;if(m==r.length){v.sort(function(e,t){return parseFloat(e.order)-parseFloat(t.order)});var u=0;for(var a in v){directionsDisplayArray[a-1]=new google.maps.DirectionsRenderer({suppressMarkers:true,polylineOptions:walkPath});directionsDisplayArray[a-1].setMap(map);if(v[a].result!=null){if(v.hasOwnProperty(a)){if(u==0)var f=v[a].result.routes[0].legs;else{f=f.concat(v[a].result.routes[0].legs)}if(i[a-1]=="MARCHE"){directionsDisplayArray[a-1].setDirections(v[a].result)}u++}}}for(var l=0;l<f.length;l++){var c="A".charCodeAt(0);c+=l;c=String.fromCharCode(c);createTanMarker(directionsDisplay1.getMap(),f[l].start_location,f[l].end_location,s[l+1]);createMarker(directionsDisplay1.getMap(),f[l].start_location,"marker"+l,"some text for marker "+l+"<br>"+f[l].start_address,c)}var l=f.length;var c="A".charCodeAt(0);c+=l;c=String.fromCharCode(c);createMarker(directionsDisplay1.getMap(),f[f.length-1].end_location,"marker"+l,"some text for the "+l+"marker<br>"+f[f.length-1].end_address,c)}}})})(E)}}function setDirectionsVoiture(e,t,n,r){var i={origin:t.coord.latitude+", "+t.coord.longitude,destination:n.coord.latitude+", "+n.coord.longitude,optimizeWaypoints:true,travelMode:google.maps.TravelMode.DRIVING};directionsService.route(i,function(e,t){if(t==google.maps.DirectionsStatus.OK){directionsDisplay.setDirections(e)}});createMarker(directionsDisplay.getMap(),new google.maps.LatLng(r.coord.latitude,r.coord.longitude),r.nom,"Cout parking "+r.cout+"<br>","parking")}function getMarkerImage(e){if(typeof e=="undefined"||e==null){e="red"}if(!icons[e]){if(e=="parking"){icons[e]=new google.maps.MarkerImage("http://img1.imagilive.com/0614/parkinggarage.png",new google.maps.Size(32,37),new google.maps.Point(0,0),new google.maps.Point(9,34))}else{icons[e]=new google.maps.MarkerImage("http://www.google.com/mapfiles/marker"+e+".png",new google.maps.Size(20,34),new google.maps.Point(0,0),new google.maps.Point(9,34))}}return icons[e]}function getPuceImageLetter(e){return"http://maps.google.com/mapfiles/kml/paddle/"+e+"-lv.png"}function clone(e){if(null==e||"object"!=typeof e)return e;var t=e.constructor();for(var n in e){if(e.hasOwnProperty(n))t[n]=e[n]}return t}var map;var initialize;var directionsService;var directionsDisplay;var directionsDisplay1;var directionsDisplayArray=[];var iconShadow=new google.maps.MarkerImage("http://www.google.com/mapfiles/shadow50.png");var icons=new Array;var iconShape={coord:[9,0,6,1,4,2,2,4,0,8,0,12,1,14,2,16,5,19,7,23,8,26,9,30,9,34,11,34,11,30,12,26,13,24,14,21,16,18,18,16,20,12,20,8,18,4,16,2,15,1,13,0],type:"poly"};var infowindow=new google.maps.InfoWindow({size:new google.maps.Size(150,50)});icons["red"]=new google.maps.MarkerImage("http://www.geocodezip.com/mapIcons/marker_red.png",new google.maps.Size(20,34),new google.maps.Point(0,0),new google.maps.Point(9,34));var drivingColor="#CC0044";var transitColor="#2BCC3D";initialize=function(){var e=new google.maps.LatLng(47.2173,-1.5534);var t={zoom:14,center:e,mapTypeId:google.maps.MapTypeId.ROADMAP,maxZoom:20};map=new google.maps.Map(document.getElementById("map"),t);drivingPath=new google.maps.Polyline({strokeColor:drivingColor,strokeOpacity:.8,strokeWeight:5,map:map});var n={path:"M 0,-1 0,1",strokeOpacity:1,scale:4};walkPath=new google.maps.Polyline({strokeColor:transitColor,strokeOpacity:0,icons:[{icon:n,offset:"0",repeat:"20px"}],strokeWeight:5,map:map});busPaths=[];directionsDisplay=new google.maps.DirectionsRenderer({suppressMarkers:false,polylineOptions:drivingPath});directionsDisplay.setMap(map);directionsService=new google.maps.DirectionsService;directionsDisplay1=new google.maps.DirectionsRenderer({suppressMarkers:false,polylineOptions:walkPath});directionsDisplay1.setMap(map)};initialize()
+var map;
+var initialize;
+var directionsService;
+var directionsDisplay;
+var directionsDisplay1;
+var directionsDisplayArray = [];
+var iconShadow = new google.maps.MarkerImage('http://www.google.com/mapfiles/shadow50.png');
+var icons = new Array();
+var iconShape = {
+	      coord: [9,0,6,1,4,2,2,4,0,8,0,12,1,14,2,16,5,19,7,23,8,26,9,30,9,34,11,34,11,30,12,26,13,24,14,21,16,18,18,16,20,12,20,8,18,4,16,2,15,1,13,0],
+	      type: 'poly'
+	  };
+var infowindow = new google.maps.InfoWindow(
+		  { 
+		    size: new google.maps.Size(150,50)
+		  });
+icons["red"] = new google.maps.MarkerImage("http://www.geocodezip.com/mapIcons/marker_red.png",
+      // This marker is 20 pixels wide by 34 pixels tall.
+      new google.maps.Size(20, 34),
+      // The origin for this image is 0,0.
+      new google.maps.Point(0,0),
+      // The anchor for this image is at 9,34.
+      new google.maps.Point(9, 34));
+
+function createMarker(map, latlng, label, html, color) {
+	// alert("createMarker("+latlng+","+label+","+html+","+color+")");
+	    var contentString = '<b>'+label+'</b><br>'+html;
+	    var marker = new google.maps.Marker({
+	        position: latlng,
+	        map: map,
+	        shadow: iconShadow,
+	        icon: getMarkerImage(color),
+	        shape: iconShape,
+	        title: label,
+	        zIndex: Math.round(latlng.lat()*-100000)<<5
+	        });
+	        marker.myname = label;
+
+	    google.maps.event.addListener(marker, 'click', function() {
+	        infowindow.setContent(contentString); 
+	        infowindow.open(map,marker);
+	        });
+	    return marker;
+	}
+
+
+function createTanMarker(map, latlng_depart,latlng_arrivee, ligne) {
+		latlng = new google.maps.LatLng((latlng_arrivee.k-latlng_depart.k)/2 + latlng_depart.k, (latlng_arrivee.A-latlng_depart.A)/2 + latlng_depart.A);
+	    var marker = new google.maps.Marker({
+	        position: latlng,
+	        map: map,
+	        icon: new google.maps.MarkerImage("/images/"+ ligne +".gif",
+	        		null, /* size is determined at runtime */
+	        	    null, /* origin is 0,0 */
+	        	    null, /* anchor is bottom center of the scaled image */
+	        	    new google.maps.Size(14, 14)),
+	        title: "Ligne",
+	        });
+
+	    return marker;
+	}
+
+var drivingColor = "#CC0044";
+var transitColor = "#2BCC3D";
+
+initialize = function() {
+	
+	
+	
+	
+
+	var latLng = new google.maps.LatLng(47.2173, -1.5534);
+	var myOptions = {
+		zoom : 14,
+		center : latLng,
+		mapTypeId : google.maps.MapTypeId.ROADMAP, // Type de carte,
+		// différentes valeurs
+		// possible HYBRID, ROADMAP,
+		// SATELLITE, TERRAIN
+		maxZoom : 20
+	};
+
+	map = new google.maps.Map(document.getElementById('map'), myOptions);
+	
+	
+	
+	drivingPath = new google.maps.Polyline({
+		strokeColor : drivingColor,
+		strokeOpacity : 0.8,
+		strokeWeight : 5,
+		map : map
+	});
+	
+	var lineSymbol = {
+		    path: 'M 0,-1 0,1',
+		    strokeOpacity: 1,
+		    scale: 4
+		  };
+	
+	walkPath = new google.maps.Polyline({
+		strokeColor : transitColor,
+		strokeOpacity : 0,
+		icons: [{
+		      icon: lineSymbol,
+		      offset: '0',
+		      repeat: '20px'
+		    }],
+		strokeWeight : 5,
+		map : map
+	});
+	
+	busPaths = [];
+	
+	directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: false,polylineOptions:drivingPath});
+	directionsDisplay.setMap(map);
+	directionsService = new google.maps.DirectionsService();
+	 
+	directionsDisplay1 = new google.maps.DirectionsRenderer({suppressMarkers: false,polylineOptions:walkPath});
+	directionsDisplay1.setMap(map);
+
+	
+
+};
+
+function setDirectionsBus(coordsBus, adresseDepart, adresseArrivee){ //récursif
+	var wayptsbus = [];
+	var wayptsbustravelmodes = [];
+	var wayptsbuslignes = [];
+	var itemsPerBatch = 10; // google API max = 10 - 1 start, 1 stop,  8 waypoints
+	var itemsCounter = 0;
+	var wayptsExist = coordsBus.length > 0;
+	var lastTravelMode;
+	
+  //Création des waypoints
+    while (wayptsExist) {
+        var subWayptsBus = [];
+        var subitemsCounter = 0;
+        var lastTravelMode = coordsBus[itemsCounter+1].travelMode;
+        var lastLigne = coordsBus[itemsCounter+1].ligne;
+        for (var j = itemsCounter; j < coordsBus.length; j++) {
+        	if (j != itemsCounter && coordsBus[j].travelMode != lastTravelMode){
+        		wayptsbuslignes.pop(); //On supprime le dernier comme on va repasser dessus la prochaine fois
+            	break;
+        	}
+        	subitemsCounter++;
+            subWayptsBus.push({
+                location: new google.maps.LatLng(coordsBus[j].latitude, coordsBus[j].longitude),
+                stopover: true            
+            });
+            wayptsbuslignes.push(coordsBus[j].ligne);
+            if (subitemsCounter == itemsPerBatch){
+            	wayptsbuslignes.pop(); //On supprime le dernier comme on va repasser dessus la prochaine fois
+                break;
+            }
+            
+        }
+
+        itemsCounter += subitemsCounter;
+        wayptsbus.push(subWayptsBus);
+        wayptsbustravelmodes.push(lastTravelMode);
+        wayptsExist = itemsCounter < coordsBus.length;
+        
+        itemsCounter--;
+    }
+   
+    // maintenant on devrait avoir array avec une liste de liste de waypoints
+    var combinedResults;
+    var unsortedResults = [{}]; // to hold the counter and the results themselves as they come back, to later sort
+    var directionsResultsReturned = 0;
+    for (var key in wayptsbus) {
+    	if (wayptsbustravelmodes[key]=="BUS" || wayptsbustravelmodes[key]=="TRAM"){
+    		var polylinepath = [];
+        	 for (var key2 in wayptsbus[key]) {
+	        	polylinepath.push(wayptsbus[key][key2].location);
+        	 }
+        	 var busPath = new google.maps.Polyline({
+        			strokeColor : transitColor,
+        			strokeOpacity : 0.8,
+        			path:polylinepath,
+        			strokeWeight : 5,
+        			map : map
+        		});
+        	 busPaths.push(busPath);
+    	}
+    }
+    
+    for (var k = 0; k < wayptsbus.length; k++) {
+    	var lastIndex = wayptsbus[k].length - 1;
+        var start = wayptsbus[k][0].location;
+        var end = wayptsbus[k][lastIndex].location;
+        
+        var waypts = [];
+        waypts = wayptsbus[k];
+        waypts.splice(0, 1);
+        waypts.splice(waypts.length - 1, 1);
+    	
+        var request = {
+  		      origin: start,
+  		      destination: end,
+  		      waypoints: waypts,
+  		      travelMode: google.maps.TravelMode.WALKING
+  		  };
+        
+        
+    (function (kk) {
+        directionsService.route(request, function (result, status) {
+            if (status == window.google.maps.DirectionsStatus.OK) {
+
+                var unsortedResult = { order: kk, result: result };
+                unsortedResults.push(unsortedResult);
+
+                directionsResultsReturned++;
+
+                if (directionsResultsReturned == wayptsbus.length) // on reçoit tous les résultats on les ajoutes à la map
+                {
+                    // on les organise dans leur bon sens
+                    unsortedResults.sort(function (a, b) { return parseFloat(a.order) - parseFloat(b.order); });
+                    var count = 0;
+                    for (var key in unsortedResults) {
+                    	directionsDisplayArray[key-1] = new google.maps.DirectionsRenderer({suppressMarkers: true,polylineOptions:walkPath});
+                    	directionsDisplayArray[key-1].setMap(map);
+                    	if (unsortedResults[key].result != null) {
+                            if (unsortedResults.hasOwnProperty(key)) {
+                            	
+                                if (count == 0) //premier resultat
+                                    var legs = unsortedResults[key].result.routes[0].legs;
+                                else {
+                                	legs = legs.concat(unsortedResults[key].result.routes[0].legs);
+                                }
+                                if (wayptsbustravelmodes[key-1]=="MARCHE"){
+                                		directionsDisplayArray[key-1].setDirections(unsortedResults[key].result);
+                                }
+                               
+                                count++;
+                            }
+                        }
+                    }
+                    
+                    for (var i=0; i < legs.length;i++){
+					  var markerletter = "A".charCodeAt(0);
+					  markerletter += i;
+                      markerletter = String.fromCharCode(markerletter);
+                      createTanMarker(directionsDisplay1.getMap(),legs[i].start_location, legs[i].end_location,wayptsbuslignes[i+1]);
+                      createMarker(directionsDisplay1.getMap(),legs[i].start_location,"marker"+i,"some text for marker "+i+"<br>"+legs[i].start_address,markerletter);
+                    }
+                    var i=legs.length;
+                    var markerletter = "A".charCodeAt(0);
+                    markerletter += i;
+                    markerletter = String.fromCharCode(markerletter);
+                    createMarker(directionsDisplay1.getMap(),legs[legs.length-1].end_location,"marker"+i,"some text for the "+i+"marker<br>"+legs[legs.length-1].end_address,markerletter);
+               }
+            }
+        });
+    })(k);
+    }
+	
+}
+
+function setDirectionsVoiture(coordsVoiture, adresseDepart, adresseArrivee, parking) {
+	
+
+	var request = {
+		      origin: adresseDepart.coord.latitude+", "+adresseDepart.coord.longitude,
+		      destination: adresseArrivee.coord.latitude+", "+adresseArrivee.coord.longitude,
+		      optimizeWaypoints: true,
+		      travelMode: google.maps.TravelMode.DRIVING
+		  };
+	
+	
+	
+	directionsService.route(request, function(response, status) {
+	    if (status == google.maps.DirectionsStatus.OK) {
+	      directionsDisplay.setDirections(response);
+	  }});
+	
+	
+	
+	createMarker(directionsDisplay.getMap(),new google.maps.LatLng(parking.coord.latitude, parking.coord.longitude),parking.nom,"Cout parking "+parking.cout+"<br>","parking")
+	/*createMarker(directionsDisplay.getMap(),new google.maps.LatLng(adresseDepart.coord.latitude, adresseDepart.coord.longitude),"Tiltesf","markersome text for depart <br>","A");
+	var markerletter = "B".charCodeAt(0);
+	/*wayptsvoiture.forEach(function(entry){
+		createMarker(directionsDisplay.getMap(),entry.location,"Tiltesf","markersome text for marker <br>",String.fromCharCode(markerletter));
+		markerletter += 1;
+	});*/
+	/*createMarker(directionsDisplay.getMap(),new google.maps.LatLng(adresseArrivee.coord.latitude, adresseArrivee.coord.longitude),"Tiltesf","markersome text for arrivee <br>",String.fromCharCode(markerletter));*/
+}
+
+function getMarkerImage(iconStr) {
+	   if ((typeof(iconStr)=="undefined") || (iconStr==null)) { 
+	      iconStr = "red"; 
+	   }
+	   if (!icons[iconStr]) {
+		  if(iconStr=="parking"){
+			  icons[iconStr] = new google.maps.MarkerImage("http://img1.imagilive.com/0614/parkinggarage.png",
+				    	// This marker is 20 pixels wide by 34 pixels tall.
+				    	 new google.maps.Size(32, 37),
+				      	// The origin for this image is 0,0.
+				      	new google.maps.Point(0,0),
+				      	// The anchor for this image is at 6,20.
+				      	new google.maps.Point(9, 34));
+		  }else{
+			icons[iconStr] = new google.maps.MarkerImage("http://www.google.com/mapfiles/marker"+ iconStr +".png",
+	    	// This marker is 20 pixels wide by 34 pixels tall.
+	    	 new google.maps.Size(20, 34),
+	      	// The origin for this image is 0,0.
+	      	new google.maps.Point(0,0),
+	      	// The anchor for this image is at 6,20.
+	      	new google.maps.Point(9, 34));
+		  }
+	   } 
+	   return icons[iconStr];
+
+	}
+
+function getPuceImageLetter(lettre){
+	return "http://maps.google.com/mapfiles/kml/paddle/"+lettre+"-lv.png"
+}
+
+function clone(obj) {
+    if (null == obj || "object" != typeof obj) return obj;
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+}
+
+
+initialize();
+
